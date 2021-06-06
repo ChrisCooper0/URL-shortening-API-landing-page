1dd9640afd71e879b5e738dbf4616252b46c1764
ChrisCooper0 <christopher.cooper@mail.com>
2021-06-06 21:12:34 +0100

    Init JS script

---
+++ script.js
@@ -0,0 +1,22 @@
+// Shorten It button
+const shorten = () => {
+  const shortenBtn = document.getElementById("shorten-btn");
+
+  // Red border and add text if no input
+
+  // Validate URL
+
+  // Sent to API
+
+  // Update UI with response
+};
+
+// URL copy button
+const clicked = () => {
+  const copyBtn = document.getElementById("copy-btn");
+
+  // Copy short-url text to clipboard
+
+  copyBtn.classList.add("copied");
+  copyBtn.innerText = "Copied!";
+};
